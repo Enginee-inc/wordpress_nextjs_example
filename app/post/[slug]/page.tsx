34c530bf789bef4fd09f81dd24e6abc294e313b7
KatHaruto <haru10.jm@gmail.com>
2025-07-03 14:18:14 +0900

    tmp

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -18,9 +18,7 @@ export async function generateStaticParams(){
 
 export default async function BlogPage({ params }: { params: { slug: string } }) {  
     // Fetch the blog post data based on the slug (you may use an API or other method)
-    const response = await fetch(`${wordpressUrl}/wp-json/wp/v2/posts/?slug=${params.slug}&_embed`, {
-      next: { revalidate: 3600 }
-    });
+    const response = await fetch(`${wordpressUrl}/wp-json/wp/v2/posts/?slug=${params.slug}&_embed`);
     const post: Post = (await response.json())[0];
 
     return (
