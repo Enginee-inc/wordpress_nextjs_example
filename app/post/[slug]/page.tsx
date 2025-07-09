b39b27099e114069c4f0425a83ce3175c4f22296
Kat <74958594+KatHaruto@users.noreply.github.com>
2025-07-09 13:19:07 +0900

    Update page.tsx

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -5,7 +5,7 @@ const wordpressUrl = process.env.WORDPRESS_URL;
 
 
 export async function generateStaticParams(){
-  const response = await fetch(`${wordpressUrl}/wp-json/wp/v2/posts?_fields[]=slug`);
+  const response = await fetch("/api/wp-json/wp/v2/posts?_fields[]=slug");
   const posts : Post[] = await response.json();
   const paths = posts.map(post => ({
     slug: post.slug 
@@ -15,7 +15,7 @@ export async function generateStaticParams(){
 
 export default async function BlogPage({ params }: { params: { slug: string } }) {  
     // Fetch the blog post data based on the slug (you may use an API or other method)
-    const response = await fetch(`${wordpressUrl}/wp-json/wp/v2/posts/?slug=${params.slug}&_embed`);
+    const response = await fetch(`/api/wp-json/wp/v2/posts/?slug=${params.slug}&_embed`);
     const post: Post = (await response.json())[0];
 
     return (
